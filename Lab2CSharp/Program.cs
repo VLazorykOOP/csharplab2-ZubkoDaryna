8a64899a1b159c61cd8cfe446197410a1f88ad4e
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2024-02-22 23:42:37 +0200

    Task1

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -1,10 +1,84 @@
 ﻿namespace Lab2CSharp
 {
-    internal class Program
+     class Program
     {
+        static void task1_1()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[n];
+
+            Console.WriteLine("Enter the elements of the array:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"a[{i}] = ");
+                myArray[i] = int.Parse(Console.ReadLine());
+            }
+                        double sum = 0;
+
+            foreach (int element in myArray)
+            {
+                sum += element;
+            }
+
+            double average = sum / n;
+
+            Console.WriteLine($"Arithmetic average of array elements: {average}");
+        
+    }
+        static void task1_2()
+        {
+
+            Console.WriteLine("Enter the size of the array:");
+            Console.WriteLine("n:");
+
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("m:");
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] myArray = new int[n, m];
+
+            Console.WriteLine("Enter the elements of the array:");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write($"a[{i},{j}] = ");
+                    myArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            double sum = 0;
+
+            foreach (int element in myArray)
+            {
+                sum += element;
+            }
+
+            double average = sum / (n * m);
+
+            Console.WriteLine($"Arithmetic average of array elements: {average}");
+        
+    }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Lab 2 CSharp");
+            Console.WriteLine("Lab2");
+            Console.WriteLine("Please input task");
+            Console.Write("Task=");
+
+            byte n = byte.Parse(Console.ReadLine());
+            switch (n) {
+                case 1: task1_1(); break;
+                case 2: task1_2(); break;
+               // case 3: task1_3(); break;
+               //case 4: task1_4(); break;
+               //case 5: task1_5(); break;
+
+            }
         }
     }
 }
