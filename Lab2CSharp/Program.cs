c7056393c54074c5890a0ced59d63afe727d84f6
ZubkoDaryna <124194246+ZubkoDaryna@users.noreply.github.com>
2024-02-23 03:08:21 +0200

    Lab2

--- Lab2CSharp/Program.cs
+++ Lab2CSharp/Program.cs
@@ -1,9 +1,10 @@
 ﻿namespace Lab2CSharp
 {
-     class Program
+    internal class Program
     {
         static void task1_1()
         {
+            double sum = 0;
             Console.WriteLine("Enter the size of the array:");
             int n = int.Parse(Console.ReadLine());
 
@@ -16,7 +17,6 @@
                 Console.Write($"a[{i}] = ");
                 myArray[i] = int.Parse(Console.ReadLine());
             }
-                        double sum = 0;
 
             foreach (int element in myArray)
             {
@@ -24,11 +24,13 @@
             }
 
             double average = sum / n;
+
             Console.WriteLine($"Arithmetic average of array elements: {average}");
         
     }
         static void task1_2()
         {
+
             Console.WriteLine("Enter the size of the array:");
             Console.WriteLine("n:");
 
@@ -55,56 +57,159 @@
             {
                 sum += element;
             }
+
             double average = sum / (n * m);
 
             Console.WriteLine($"Arithmetic average of array elements: {average}");
+        
     }
- static void task1_3()
- {
-     Console.WriteLine("Enter the size of the array:");
-     int n = int.Parse(Console.ReadLine());
+        static void task1_3()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int n = int.Parse(Console.ReadLine());
 
-     double[] myArray = new double[n];
+            double[] myArray = new double[n];
 
-     Console.WriteLine("Enter the elements:");
+            Console.WriteLine("Enter the elements:");
 
-     for (int i = 0; i < n; i++)
-     {
-         Console.Write($"a[{i}] = ");
-         myArray[i] = double.Parse(Console.ReadLine());
-     }
-     double max = myArray[0];
-     for (int i = 1; i<n; i++)
-     {
-         if (myArray[i] > max)
-         {
-             max = myArray[i];
-         }
-     }
-     Console.WriteLine("Numbers of all elements that do not match the maximum:");
-     for (int i = 0; i < n; i++)
-     {
-         if (myArray[i] != max)
-         {
-             Console.WriteLine(i);
-         }
-     }
- }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"a[{i}] = ");
+                myArray[i] = double.Parse(Console.ReadLine());
+            }
+            double max = myArray[0];
+            for (int i = 1; i<n; i++)
+            {
+                if (myArray[i] > max)
+                {
+                    max = myArray[i];
+                }
+            }
+            Console.WriteLine("Numbers of all elements that do not match the maximum:");
+            for (int i = 0; i < n; i++)
+            {
+                if (myArray[i] != max)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void task1_4()
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] myArray = new int[n, n];
+
+            Console.WriteLine("Enter the elements of the array:");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"a[{i},{j}] = ");
+                    myArray[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Original array:");
+            PrintArray(myArray);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+                    int temp = myArray[i, j];
+                    myArray[i, j] = myArray[i, n - j - 1];
+                    myArray[i, n - j - 1] = temp;
+                }
+            }
+
+            Console.WriteLine("Array after swapping columns:");
+            PrintArray(myArray);
+        }
+        static void PrintArray(int[,] array)
+        {//кількість елементів у першому розмірі масиву(рядки)
+            int n = array.GetLength(0); 
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{array[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void task1_5()
+        {
+            Console.WriteLine("Enter the number of rows in the array:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] myArray = new int[n][];
+
+            Console.WriteLine("Enter the number of elements in each row:");
+            int[] m = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"m[{i}] = ");
+                m[i] = int.Parse(Console.ReadLine());
+                // масив цілих чисел з розміром, який вказує користувач
+                myArray[i] = new int[m[i]]; 
+            }
+
+            Console.WriteLine("Enter the elements of the array:");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m[i]; j++)
+                {
+                    Console.Write($"a[{i},{j}] = ");
+                    myArray[i][j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Enter the number to compare:");
+            int compareNumber = int.Parse(Console.ReadLine());
+
+            int[] resultArray = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < m[i]; j++)
+                {
+                    if (myArray[i][j] > compareNumber)
+                    {
+                        count++;
+                    }
+                }
+                resultArray[i] = count;
+            }
+
+            Console.WriteLine("Result array:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Row {i + 1}: {resultArray[i]} elements greater than {compareNumber}");
+            }
+        }
+   
         static void Main(string[] args)
         {
             Console.WriteLine("Lab2");
             Console.WriteLine("Please input task");
-            Console.Write("Task=");
+            Console.Write("n=");
 
             byte n = byte.Parse(Console.ReadLine());
             switch (n) {
                 case 1: task1_1(); break;
                 case 2: task1_2(); break;
                 case 3: task1_3(); break;
-               //case 4: task1_4(); break;
-               //case 5: task1_5(); break;
+                case 4: task1_4(); break;
+                case 5: task1_5(); break;
 
             }
         }
-    }
+     }
 }
+ 
+
